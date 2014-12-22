cd6274f20058d9ae32d744ad3573035f1566f0df
Marco Rougeth <marco@rougeth.com>
2014-12-22 15:23:16 -0200

    Created base62 converter class

---
+++ test_base62.py
@@ -0,0 +1,11 @@
+from shortener import Base62
+
+
+def test_base62_to_decimal():
+    n = Base62()
+    assert n.to_decinal('abc') == '39134'
+
+
+def test_base62_from_decimal():
+    n = Base62()
+    assert n.from_decinal('39134') == 'abc'
