aa152c5ec4089f26d4064904617818e0db3c2a8f
Marco Rougeth <marco@rougeth.com>
2014-12-23 09:49:16 -0200

    Created basic url converter view

--- piny.py
+++ piny.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
+from shortener import Base62
+
 
 app = Flask(__name__)
 
@@ -7,6 +9,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello World'
+
+
+@app.route('/<url_id>')
+def redirect(url_id):
+    url = Base62()
+    return url.to_decinal(url_id)
 
 
 if __name__ == '__main__':
