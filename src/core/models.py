1b212a5a44375af5cb7d43c4c2950b9362a0cc70
Marco Rougeth <marco@rougeth.com>
2015-02-07 17:22:56 -0200

    Registered Url model in admin and changed its __str and __repr methods

--- src/core/models.py
+++ src/core/models.py
@@ -1,7 +1,23 @@
 from django.db import models
 
+from baco import Baco
+
 
 class Url(models.Model):
     original_url = models.URLField(max_length=300)
     custom_shortened_url = models.CharField(max_length=30, null=True)
     creation_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.custom_shortened_url:
+            return '{}: {}'.format(self.custom_shortened_url,
+                                   self.original_url)
+        else:
+            return '{}: {}'.format(Baco.to_62(self.id),
+                                   self.original_url)
+
+    def __repr__(self):
+        if self.custom_shortened_url:
+            return '<Url: %>', self.custom_shortened_url
+        else:
+            return '<Url: %>', Baco.to_62(self.id)
